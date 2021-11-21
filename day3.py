bdea218eeffcbfb992939a1e67f4f6e63fb88e42
adityapshetty <94469467+adityapshetty@users.noreply.github.com>
2021-11-21 16:26:02 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+print(f"the addition and subtraction of {a} and {b} are {add(a,b)} and {sub(a,b)} respectively")
